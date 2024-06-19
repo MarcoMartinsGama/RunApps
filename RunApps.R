1987412ff0fc6c0fbc220f099b056924a1aedf72
MarcoMartinsGama <marcomartinsgama@gmail.com>
2024-06-18 20:22:26 -0700

    Code

---
+++ RunApps.R
@@ -0,0 +1,6 @@
+library(shiny)
+
+
+runGitHub(repo = "SummaryToKeys",username = "MarcoMartinsGama",ref = "main")
+runGitHub(repo = "ContrastGenerator",username = "MarcoMartinsGama",ref = "main")
+  
