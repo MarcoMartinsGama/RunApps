d2f6c44e4de804221cd78bcd1e92563b203e60b7
MarcoMartinsGama <marcomartinsgama@gmail.com>
2024-07-31 19:00:17 -0700

    V1.0

--- RunApps.R
+++ RunApps.R
@@ -6,4 +6,4 @@ runGitHub(repo = "QC-and-SAINTexpress-inputs",username = "MarcoMartinsGama",ref 
 runGitHub(repo = "SAINTexpress",username = "MarcoMartinsGama",ref = "main")
 runGitHub(repo = "MSstats",username = "MarcoMartinsGama",ref = "main")
 runGitHub(repo = "T-test",username = "MarcoMartinsGama",ref = "main")
-runGitHub(repo = "Final_Dataset_Generator",username = "MarcoMartinsGama",ref = "main")
+runGitHub(repo = "Control_Dataset_Generator",username = "MarcoMartinsGama",ref = "main")
